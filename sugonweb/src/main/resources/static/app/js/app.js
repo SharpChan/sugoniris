cdfea3fb57710d2b3454085422ea01d9ad073ab5
SharpChan <chengfeng3539@126.com>
2021-07-07 00:17:18 +0800

    页面注册

--- sugonweb/src/main/resources/static/app/js/app.js
+++ sugonweb/src/main/resources/static/app/js/app.js
@@ -1172,10 +1172,156 @@ App.service('myservice', function($window,$state,$http) {
 App.controller("userRoleController", function ($http,$timeout,$scope,$state,
                                                    myservice) {
 
-
+    var vm = this;
+    vm.tree = [
+        {
+            "id":"1",
+            "pid":"0",
+            "name":"家用电器",
+            "children":[
+                {
+                    "id":"4",
+                    "pid":"1",
+                    "name":"大家电",
+                    "children":[
+                        {
+                            "id":"7",
+                            "pid":"4",
+                            "name":"空调",
+                            "children":[
+                                {
+                                    "id":"15",
+                                    "pid":"7",
+                                    "name":"海尔空调"
+                                },
+                                {
+                                    "id":"16",
+                                    "pid":"7",
+                                    "name":"美的空调"
+                                }
+                            ]
+                        },
+                        {
+                            "id":"8",
+                            "pid":"4",
+                            "name":"冰箱"
+                        },
+                        {
+                            "id":"9",
+                            "pid":"4",
+                            "name":"洗衣机"
+                        },
+                        {
+                            "id":"10",
+                            "pid":"4",
+                            "name":"热水器"
+                        }
+                    ]
+                },
+                {
+                    "id":"5",
+                    "pid":"1",
+                    "name":"生活电器",
+                    "children":[
+                        {
+                            "id":"19",
+                            "pid":"5",
+                            "name":"加湿器"
+                        },
+                        {
+                            "id":"20",
+                            "pid":"5",
+                            "name":"电熨斗"
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "id":"2",
+            "pid":"0",
+            "name":"服饰",
+            "children":[
+                {
+                    "id":"13",
+                    "pid":"2",
+                    "name":"男装"
+                },
+                {
+                    "id":"14",
+                    "pid":"2",
+                    "name":"女装"
+                }
+            ]
+        },
+        {
+            "id":"3",
+            "pid":"0",
+            "name":"化妆",
+            "children":[
+                {
+                    "id":"11",
+                    "pid":"3",
+                    "name":"面部护理"
+                },
+                {
+                    "id":"12",
+                    "pid":"3",
+                    "name":"口腔护理"
+                }
+            ]
+        }
+    ];
 
 });
 
+App.directive('treeView',[function(){
+
+    return {
+        restrict: 'E',
+        templateUrl: '/treeView.html',
+        scope: {
+            treeData: '=',
+            canChecked: '=',
+            textField: '@',
+            itemClicked: '&',
+            itemCheckedChanged: '&',
+            itemTemplateUrl: '@'
+        },
+        controller:['$scope', function($scope){
+            $scope.itemExpended = function(item, $event){
+                item.$$isExpend = ! item.$$isExpend;
+                $event.stopPropagation();
+            };
+
+            $scope.getItemIcon = function(item){
+                var isLeaf = $scope.isLeaf(item);
+
+                if(isLeaf){
+                    return 'fa fa-leaf';
+                }
+
+                return item.$$isExpend ? 'fa fa-minus': 'fa fa-plus';
+            };
+
+            $scope.isLeaf = function(item){
+                return !item.children || !item.children.length;
+            };
+
+            $scope.chk = function(callback , item){
+                var itemId = item.id;
+
+            };
+
+            $scope.warpCallback = function(callback, item, $event){
+                ($scope[callback] || angular.noop)({
+                    $item:item,
+                    $event:$event
+                });
+            };
+        }]
+    };
+}]);
 
 App.controller("pageRegisterController", function ($http,$timeout,$scope,$state,
                                                    myservice) {
@@ -8513,6 +8659,7 @@ App.controller('WordCloudController', ['$scope', function ($scope) {
 
 }]);
 
+
 /**=========================================================
  * Module: anchor.js
  * Disables null anchor behavior
@@ -10555,6 +10702,8 @@ myApp.controller('oneOfMyOwnController', ["$scope", function($scope) {
 myApp.directive('oneOfMyOwnDirectives', function() {
   /*directive code*/
 });
+
+
 
 myApp.config(["$stateProvider", function($stateProvider /* ... */) {
   /* specific routes here (see file config.js) */
