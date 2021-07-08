9981c3f70b3bcff25cbdd06b8bdc2b5ba9f80c8b
SharpChan <chengfeng3539@126.com>
2021-07-09 00:07:14 +0800

    角色页面配置

---
+++ sugondata/src/main/java/com/sugon/iris/sugondata/jdbcTemplate/impl/system/RolePageServiceDaoImpl.java
@@ -0,0 +1,76 @@
+package com.sugon.iris.sugondata.jdbcTemplate.impl.system;
+
+import com.sugon.iris.sugondata.jdbcTemplate.intf.system.RolePageServiceDao;
+import com.sugon.iris.sugondomain.beans.baseBeans.Error;
+import com.sugon.iris.sugondomain.entities.jdbcTemplateEntity.systemEntities.RolePageEntity;
+import com.sugon.iris.sugondomain.enums.ErrorCode_Enum;
+import org.apache.commons.lang.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementSetter;
+import org.springframework.util.CollectionUtils;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.sql.Types;
+import java.util.List;
+
+public class RolePageServiceDaoImpl implements RolePageServiceDao {
+    private static final Logger LOGGER = LogManager.getLogger(MenuServiceDaoImpl.class);
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public int[] saveRolePages(List<RolePageEntity> rolePageEntityList, List<Error> errorList) {
+
+        int[] result=null;
+
+        if(CollectionUtils.isEmpty(rolePageEntityList)){
+            return result;
+        }
+        String sql = "insert into sys_role_page(role_id,menu_id,create_time) " +
+                "values(?,?,?)";
+
+        try{
+            result=jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter(){
+
+
+
+                @Override
+                public int getBatchSize() {
+                    return rolePageEntityList.size();
+                }
+
+                @Override
+                public void setValues(PreparedStatement ps, int i) throws SQLException {
+                    ps.setLong(1, rolePageEntityList.get(i).getRoleId());
+                    ps.setLong(2, rolePageEntityList.get(i).getMenuId());
+                    ps.setTimestamp(3, new Timestamp( rolePageEntityList.get(i).getCreateTime().getTime()));
+                }
+            });
+        }catch (Exception e){
+            LOGGER.info("{}-{}","插入表sys_role_page失败",e);
+            errorList.add(new Error(ErrorCode_Enum.SYS_DB_001.getCode(),"插入sys_role_page表出错",e.toString()));
+        }
+        return result;
+
+    }
+
+    @Override
+    public int[] deleteRolePages(List<Object[]> idList, List<Error> errorList) {
+        int[] ints = null;
+        //1.创建sql语句
+        String sql = "delete from sys_role_page where id=?";
+        //2.调用方法实现
+        try {
+            ints = jdbcTemplate.batchUpdate(sql, idList);
+        }catch (Exception e){
+            errorList.add(new Error(ErrorCode_Enum.SYS_DB_001.getCode(),"删除sys_role_page表出错",e.toString()));
+        }
+        return ints;
+    }
+}
--- sugondata/src/main/java/com/sugon/iris/sugondata/jdbcTemplate/intf/system/RolePageServiceDao.java
+++ sugondata/src/main/java/com/sugon/iris/sugondata/jdbcTemplate/intf/system/RolePageServiceDao.java
@@ -1,6 +1,13 @@
 package com.sugon.iris.sugondata.jdbcTemplate.intf.system;
 
+import com.sugon.iris.sugondomain.beans.baseBeans.Error;
+import com.sugon.iris.sugondomain.entities.jdbcTemplateEntity.systemEntities.RolePageEntity;
+import java.util.List;
+
 public interface RolePageServiceDao {
 
+    int[] saveRolePages(List<RolePageEntity> rolePageEntityList, List<Error> errorList);
+
+    int[] deleteRolePages(List<Object[]> idList,List<Error> errorList);
 
 }
